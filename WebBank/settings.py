580f491ed08e49d28bca92513f04c28a320fe06a
Saurabh <saurabhlalwani4488@gmail.com>
2021-06-14 21:45:51 +0530

    sixth commit

--- WebBank/settings.py
+++ WebBank/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e9f+8)@s%$qlge@%lvn7qf7#g=^d+6-9p(z77yzg7xmz8!_nlw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['heypay-bank.herokuapp.com','127.0.0.1']
 
